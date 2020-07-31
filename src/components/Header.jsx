600b34028299c303e7e918eb7489cba65c22e633
jainsejal <sejaljain1194@gmail.com>
2020-07-31 16:17:08 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Reactdom from "react-dom"; 
+
+//3. Create a Header.jsx component that renders a <header> element
+//to show the Keeper App name in an <h1>.
+function Header(){
+  return (
+  <header>
+  <h1>Keeper</h1>
+  </header>
+  )
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+//1. Create a new React app.
+
+import React from "react";
+import Reactdom from "react-dom";
+import App from "./components/App";
+
+Reactdom.render(<App /> ,document.getElementById("root")
+)
+
+//2. Create a App.jsx component.
+
+
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
